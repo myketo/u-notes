d6d5f6890451ac21c302c9f35bbf640740acb1c3
myketo <mykys99@gmail.com>
2023-05-17 16:17:23 +0200

    Add basic packages and configuration

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/vue'
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  globals: {
+    $nuxt: true
+  }
+};
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/color-mode",
+		"nuxt-icon",
+		"@pinia/nuxt",
+		"@nuxtjs/device",
+		"@nuxt/image-edge",
+		"@formkit/nuxt",
+	],
 
-})
+	pinia: {
+		autoImports: ["defineStore"],
+	},
+
+	devtools: {
+		enabled: true,
+	},
+});
